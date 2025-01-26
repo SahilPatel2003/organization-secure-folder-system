f3cdcc12ce3ed6fb227a07211548b726919dec67
Sahil Mandani <85072907+SahilPatel2003@users.noreply.github.com>
2025-01-27 01:00:51 +0530

    Add files via upload

---
+++ usecases/index.js
@@ -0,0 +1,13 @@
+const allUserUsecases = require("./user");
+const allRoleUsecases = require("./role");
+const allFolderuasecases = require("./folder");
+const allFileUsecases = require("./file");
+const allOrganizationUsecases = require("./organization");
+
+module.exports = Object.freeze({
+  allUserUsecases,
+  allRoleUsecases,
+  allFolderuasecases,
+  allFileUsecases,
+  allOrganizationUsecases,
+});
